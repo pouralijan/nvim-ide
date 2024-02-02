ac843f6496d82a76bbd8212a91013ec7d88217ea
Hassan Pouralijan <pouralijan@gmail.com>
2024-02-02 18:28:54 +0330

    feat: remove colorscheme selector and use telescop current-theme file.

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -3,16 +3,7 @@ require("core.lazy")
 require("core.keymaps")
 
 
--- replace these with a function that can change the color scheme.
-local colorscheme = "darkplus"
-local status_ok, _ = pcall (vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
-
-local current_theme_status_ok, lspconfig = pcall(require, "current-theme")
+local current_theme_status_ok, _ = pcall(require, "current-theme")
 if not current_theme_status_ok then
 	return
 end
-
