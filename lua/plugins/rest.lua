2605afd60e8b39ad15fc8a96a3885598972f9149
Hassan Pouralijan <pouralijan@gmail.com>
2024-06-04 06:26:43 +0330

    feat: Update rest.

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -2,13 +2,19 @@ return {
 	{
 		"vhyrro/luarocks.nvim",
 		priority = 1000,
+		lazy = false,
 		config = true,
+		opts = {
+			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+		},
 	},
 	{
 		lazy = false,
 		"rest-nvim/rest.nvim",
 		ft = "http",
-		dependencies = { "luarocks.nvim" },
+		dependencies = {
+			"luarocks.nvim",
+		},
 		opts = {
 
 			keybinds = {
